a778b3dc4a2c31614cac5745b8034f1ca0fc5030
Benjamin Tan Wei Hao <benjamintanweihao@gmail.com>
2014-08-03 17:56:11 +0800

    retab.

--- spec/features/home/list_products_spec.rb
+++ spec/features/home/list_products_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 feature 'Home Page' do
-	include_context 'custom products'
+  include_context 'custom products'
 
-	let(:home_page) { HomePage.new }
+  let(:home_page) { HomePage.new }
 
-	before do
-		home_page.load
-	end
+  before do
+    home_page.load
+  end
 
-	scenario 'enters home page' do
-		expect(home_page.product_permalinks).to include('ruby-on-rails-mug', 'ruby-on-rails-tote')
-	end
+  scenario 'enters home page' do
+    expect(home_page.product_permalinks).to include('ruby-on-rails-mug', 'ruby-on-rails-tote')
+  end
 
-	scenario 'clicks on a product' do
-		product_link = home_page.product_links.first
-		product_page = home_page.click_on_product_link(product_link)
+  scenario 'clicks on a product' do
+    product_link = home_page.product_links.first
+    product_page = home_page.click_on_product_link(product_link)
 
-		expect(product_page).to be_displayed
+    expect(product_page).to be_displayed
 
-		expect(product_page.title).not_to be_empty
-		expect(product_page.description).not_to be_empty
-		expect(product_page.price).not_to be_empty
-	end
+    expect(product_page.title).not_to be_empty
+    expect(product_page.description).not_to be_empty
+    expect(product_page.price).not_to be_empty
+  end
 
 end
--- spec/features/home/search_products_spec.rb
+++ spec/features/home/search_products_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 feature 'Product Search' do
-	include_context 'custom products'
+  include_context 'custom products'
 
-	let(:home_page) { HomePage.new }
+  let(:home_page) { HomePage.new }
 
-	before do
-		home_page.load
-	end
+  before do
+    home_page.load
+  end
 
-	scenario 'a search term is entered' do
-		search_results = home_page.search_for('jersey')
-		expect(search_results.products.size).to eq(3)
-	end
+  scenario 'a search term is entered' do
+    search_results = home_page.search_for('jersey')
+    expect(search_results.products.size).to eq(3)
+  end
 
-	scenario 'search limited by dropdown' do
-		search_results = home_page.search_for('mug', 'Categories')
-		expect(search_results.products.size).to eq(1)
-	end
+  scenario 'search limited by dropdown' do
+    search_results = home_page.search_for('mug', 'Categories')
+    expect(search_results.products.size).to eq(1)
+  end
 
 end
